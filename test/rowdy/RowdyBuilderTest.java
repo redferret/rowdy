9a38a7c14b1fdf789c6e8718bae28009411278d5
Richard DeSilvey <rdesilvey@gmail.com>
2018-01-29 18:02:01 -0700

    More Testing
    Added a TestUtils
    Added more unit tests for all the basic forms of statements

--- test/rowdy/RowdyBuilderTest.java
+++ test/rowdy/RowdyBuilderTest.java
@@ -1,11 +1,10 @@
 
 package rowdy;
 
-import java.io.IOException;
-import static org.junit.Assert.*;
 import org.junit.Test;
 import static rowdy.Rowdy.*;
-
+import static rowdy.testUtils.TestUtils.*;
+import static org.junit.Assert.*;
 
 /**
  *
@@ -13,24 +12,15 @@ import static rowdy.Rowdy.*;
  */
 public class RowdyBuilderTest {
   
-  private static final RowdyLexer parser = 
-          new RowdyLexer(TERMINALS, SPECIAL_SYMBOLS, ID, CONST);
-  private static final Language rowdy = 
-          Language.build(GRAMMAR, TERMINALS, NONTERMINALS);
-  private static final RowdyBuilder builder = 
-          RowdyBuilder.getBuilder(rowdy);
-  
   /**
    * Test of getProgram method, of class RowdyBuilder.
    */
   @Test
   public void testGetProgramAsSingleLine() {
     
-    String testProgram = "main = func(){}";
+    String testCode = "main = func(){}";
     
-    parser.parseLine(testProgram);
-    builder.buildAsSingleLine(parser);
-    Node root = builder.getProgram();
+    Node root = getRoot(testCode);
     assertNotNull(root);
     String actual = root.symbol().getSymbolAsString();
     String expected = "stmt-list";
@@ -44,9 +34,9 @@ public class RowdyBuilderTest {
 
   @Test
   public void testGetProgramAsProgram() {
-    String testProgram = "func main(){}";
+    String testCode = "func main(){}";
     
-    parser.parseLine(testProgram);
+    parser.parseLine(testCode);
     builder.build(parser);
     Node root = builder.getProgram();
     assertNotNull(root);
@@ -62,39 +52,90 @@ public class RowdyBuilderTest {
   }
   
   @Test
-  public void testAssignmentStatement() {
-    String testProgram = "a = 100";
-    
-    parser.parseLine(testProgram);
-    builder.buildAsSingleLine(parser);
-    Node root = builder.getProgram();
-    assertNotNull(root);
-    
-    Node statement = getFromAndTestNotNull(root, STATEMENT);
-    Node assignStmt = getFromAndTestNotNull(statement, ASSIGN_STMT);
+  public void testAssignStatement() {
+    String testCode = "a = 100";
+    Node assignStmt = getTestStatement(testCode, ASSIGN_STMT);
     
     getAndTestSymbol(assignStmt, ID, "ID");
     getAndTestSymbol(assignStmt, BECOMES, "=");
     getAndTestSymbol(assignStmt, EXPRESSION, "expr");
   }
   
-  private Node getFromAndTestNotNull(Node from, int id) {
-    Node toFetch = from.get(id, false);
-    assertNotNull(toFetch);
-    return toFetch;
+  @Test
+  public void testLoopStatement() {
+    String testCode = "loop x: {}";
+    Node loopStmt = getTestStatement(testCode, LOOP_STMT);
+    
+    getAndTestSymbol(loopStmt, LOOP, "loop");
+    getAndTestSymbol(loopStmt, ID, "ID");
+    getAndTestSymbol(loopStmt, COLON, ":");
+    getAndTestSymbol(loopStmt, STMT_BLOCK, "stmt-block");
   }
   
-  private void testForTerminal(Node terminal, String expected) {
-    assertNotNull(terminal);
-    String actual = ((Terminal)terminal.symbol()).getName();
-    assertEquals("No main found",expected, actual);
+  @Test
+  public void testIfStatement() {
+    String testCode = "if true {} else {}";
+    Node ifStmt = getTestStatement(testCode, IF_STMT);
+    
+    getAndTestSymbol(ifStmt, IF, "if");
+    getAndTestSymbol(ifStmt, EXPRESSION, "expr");
+    getAndTestSymbol(ifStmt, STMT_BLOCK, "stmt-block");
+    getAndTestSymbol(ifStmt, ELSE_PART, "else-part");
   }
   
-  private Node getAndTestSymbol(Node from, int nodeId, String expected){
-    Node toFetch = getFromAndTestNotNull(from, nodeId);
-    String actual = toFetch.symbol().getSymbolAsString();
-    assertEquals(expected, actual);
-    return toFetch;
+  @Test
+  public void testBreakStatement() {
+    String testCode = "break x";
+    Node breakStmt = getTestStatement(testCode, BREAK_STMT);
+    
+    getAndTestSymbol(breakStmt, BREAK, "break");
+    Node idOpt = getAndTestSymbol(breakStmt, ID_OPTION, "id-option");
+    Node id = getAndTestSymbol(idOpt, ID, "ID");
+    testForTerminal(id, "x");
+  }
+  
+  @Test
+  public void testReturnStatement() {
+    String testCode = "return 100";
+    Node returnStmt = getTestStatement(testCode, RETURN_STMT);
+    
+    getAndTestSymbol(returnStmt, RETURN, "return");
+    getAndTestSymbol(returnStmt, EXPRESSION, "expr");
+  }
+  
+  @Test
+  public void testFunctionCallStatement() {
+    String testCode = "->function()";
+    Node functionCall = getTestStatement(testCode, FUNC_CALL);
+    
+    getAndTestSymbol(functionCall, CALL, "->");
+    Node id = getAndTestSymbol(functionCall, ID, "ID");
+    testForTerminal(id, "function");
+    getAndTestSymbol(functionCall, OPENPAREN, "(");
+    getAndTestSymbol(functionCall, EXPRESSION, "expr");
+    getAndTestSymbol(functionCall, EXPR_LIST, "expr-list");
+    getAndTestSymbol(functionCall, CLOSEDPAREN, ")");
+  }
+  
+  @Test
+  public void testReadStatement() {
+    String testCode = "read testID";
+    Node readStmt = getTestStatement(testCode, READ_STMT);
+    
+    getAndTestSymbol(readStmt, READ, "read");
+    Node id = getAndTestSymbol(readStmt, ID, "ID");
+    testForTerminal(id, "testID");
+    getAndTestSymbol(readStmt, PARAMS_TAIL, "params-tail");
+  }
+  
+  @Test
+  public void testPrintStatement() {
+    String testCode = "print testID";
+    Node printStmt = getTestStatement(testCode, PRINT_STMT);
+    
+    getAndTestSymbol(printStmt, PRINT, "print");
+    getAndTestSymbol(printStmt, EXPRESSION, "expr");
+    getAndTestSymbol(printStmt, EXPR_LIST, "expr-list");
   }
   
 }
