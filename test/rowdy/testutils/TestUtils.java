fa33087456fea06fdc3859db637fb91e620091f0
Richard DeSilvey <rdesilvey@gmail.com>
2018-02-07 20:44:38 -0700

    Test utils fix
    Removed test utils

---
+++ test/rowdy/testutils/TestUtils.java
@@ -0,0 +1,32 @@
+
+package rowdy.testutils;
+
+import growdy.GRBuilder;
+import growdy.GRowdy;
+import growdy.Node;
+import growdy.exceptions.AmbiguousGrammarException;
+import growdy.exceptions.ParseException;
+import growdy.exceptions.SyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static org.junit.Assert.fail;
+import static rowdy.Rowdy.getBuilder;
+
+/**
+ *
+ * @author Richard
+ */
+public class TestUtils {
+  
+  public static Node getTestStatement(String sourceCode, int programNode) {
+    GRBuilder grBuilder = getBuilder();
+    GRowdy growdy = GRowdy.getInstance(grBuilder);
+    try {
+      growdy.buildFromString(sourceCode, programNode);
+    } catch (ParseException | SyntaxException | AmbiguousGrammarException ex) {
+      fail("Unable to load or build grammar " + ex.getLocalizedMessage());
+    }
+    return growdy.getProgram();
+  }
+  
+}
