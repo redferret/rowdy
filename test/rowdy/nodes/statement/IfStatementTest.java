d3f23aabba9ed3929d403aaa05f536938fd31021
Richard DeSilvey <rdesilvey@gmail.com>
2018-11-28 16:34:27 -0700

    Added new test case for If Statements
    Tests the full if-elseif-else statement blocks

--- test/rowdy/nodes/statement/IfStatementTest.java
+++ test/rowdy/nodes/statement/IfStatementTest.java
@@ -6,12 +6,12 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import rowdy.Value;
 import rowdy.exceptions.ConstantReassignmentException;
-import rowdy.nodes.statement.IfStatement;
 import static rowdy.testlang.lang.RowdyGrammarConstants.IF_STMT;
+import static rowdy.testlang.lang.RowdyGrammarConstants.LOOP_STMT;
 import static rowdy.testutils.TestUtils.getTestStatement;
-import static junit.framework.TestCase.assertTrue;
 import static rowdy.testutils.TestUtils.fetch;
 import static rowdy.testutils.TestUtils.isset;
+import static junit.framework.TestCase.assertTrue;
 /**
  *
  * @author Richard
@@ -30,7 +30,7 @@ public class IfStatementTest extends TestCase {
   /**
    * Test of execute method, of class IfStatement.
    */
-  public void testExecute() throws ConstantReassignmentException {
+  public void testForTrueAndFalseOnIfElse() throws ConstantReassignmentException {
     String testCode = "if 0 == 0 {a = 1 as int} else {a = 0 as int}";
     
     IfStatement instance = (IfStatement) getTestStatement(testCode, IF_STMT);
@@ -44,6 +44,38 @@ public class IfStatementTest extends TestCase {
     Integer actual = (Integer) varResult.getValue();
     Integer expected = 1;
     assertEquals("ID 'a' not set correctly", expected, actual);
+  }
+  
+  public void testFullIfElseIf() throws ConstantReassignmentException {
+    String testCode = 
+            "  loop test: {"
+            + " if (test == 0) {"
+            + "   a = 0 as int"
+            + " } else if (test == 1) {"
+            + "   b = 1 as int"
+            + " } else {"
+            + "   c = 2"
+            + "   break test"
+            + " } "
+            + " test++"
+            + "}";
+    
+    LoopStatement instance = (LoopStatement) getTestStatement(testCode, LOOP_STMT);
+    
+    instance.execute();
+    
+    String[] vars = {"a", "b", "c"};
+    Integer[] expectedValues = {0, 1, 2};
+    
+    for (int i = 0; i < 3; i++) {
+      assertTrue("Variable '"+vars[i]+"' should exist", isset(vars[i]));
+      
+      Value varResult = fetch(vars[i]);
+      Integer actual = (Integer) varResult.getValue();
+      Integer expected = expectedValues[i];
+      assertEquals("ID '"+vars[i]+"' not set correctly", expected, actual);
+    }
+    
   }
   
 }
